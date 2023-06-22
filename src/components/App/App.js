93a3484a8d4e7d12dee8f876fb2727aefb2700c3
FelipeCardozo96 <felipemcr996@gmail.com>
2023-06-22 16:31:06 -0300

    Se agregó useEffect a la lógica del localStorage. Ademas de un icono de carga animado

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,21 +4,29 @@ import "./App.css";
 /* import { Background } from "../Background/Background"; */
 import { useLocalStorage } from './useLocalStorage';
 
+
+/* localStorage.removeItem('TODOS_V1'); */
+
 /* const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el Curso de Intro a React.js', completed: false },
   { text: 'Llorar con la Llorona', completed: false },
   { text: 'LALALALALA', completed: false },
   { text: 'Usar estados derivados', completed: true },
-]; */
+];
 
-/* localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
-localStorage.removeItem('TODOS_V1'); */
+localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos)); */
+
 
 
 function App() {
 
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+    } = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(
@@ -54,6 +62,8 @@ function App() {
   
   return (
     <AppUi
+    loading={loading}
+    error={error}
     completedTodos={completedTodos}
     totalTodos={totalTodos}
     searchValue={searchValue}
--- src/components/App/AppUi.js
+++ src/components/App/AppUi.js
@@ -4,8 +4,11 @@ import { TodoSearch } from "../TodoSearch/TodoSearch";
 import { TodoList } from "../TodoList/TodoList";
 import { TodoItem } from "../TodoItem/TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton/CreateTodoButton";
+import { Loading } from "../App/Loading"
 
 function AppUi({
+    loading,
+    error,
     completedTodos,
     totalTodos,
     searchValue,
@@ -26,6 +29,10 @@ function AppUi({
           />
     
           <TodoList>
+            {loading && <Loading />}
+            {error && <p>Ocurrió un error.</p>}
+            {(!loading && searchedTodos.length == 0) && <p>Crea tu primera tarea</p>}
+            
             {searchedTodos.map(todo => (
               <TodoItem
                 key={todo.text}
